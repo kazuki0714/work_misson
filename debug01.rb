e94e70e939c62f19c94d29fb1e4accbbfb174fc9
Kazuki_Ouchi <kazukiouchi0714@gmail.com>
2019-07-14 18:37:27 +0900

    デバッグの基本①

--- debug01.rb
+++ debug01.rb
@@ -1,5 +1,6 @@
 class Animal
   def bark
+    p '----------1-----------'
     p 'Yeah, it’s barking.'
   end
 end
@@ -8,6 +9,7 @@ class Dog < Animal
   attr_accessor :name, :age
 
   def initialize(name, age=1)
+    p '----------2-----------'
     @name = name
     @age = age
   end
@@ -16,28 +18,34 @@ end
 class MechaDog < Dog
 
   def initialize(name, age=1)
+    p '----------3-----------'
     super(name, age=1)
     @data = { 'apache' => 'apache', 'bsd' => 'mit', 'chef' => 'apache' }
+    p '----------4-----------'
+
   end
 
   def proc arg
     path = (arg.split[0]).split('/')[1..-1]
+    p '----------5-----------'
     if path.nil?
       keys=[]
       @data.each do |key, _value|
         keys << key
       end
+      p '----------6-----------'
       p keys
     elsif path.size == 2
       @data[path[0].chomp] = path[1]
+      p '----------7-----------'
       p path[1]
     else
+      p '----------8-----------'
       p path[0] + " => " + @data[path[0]].to_s
+
     end
   end
-
 end
-
 
 mdog = MechaDog.new('tom')
 mdog.bark
